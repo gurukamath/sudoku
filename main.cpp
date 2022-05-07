3ff64187f1a5b3a022f2b79b08068a9d13bfab98
Guruprasad Kamath <guru241987@gmail.com>
2022-05-07 21:18:10 +0200

    add Sudoku initialisation list

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int main()
     Sudoku new_sudoku("example.txt");
     // Sudoku new_sudoku;
 
-    std::cout << new_sudoku.is_solved() << std::endl;
+    // std::cout << new_sudoku.is_solved() << std::endl;
 
-    // new_sudoku.print_sudoku();
+    new_sudoku.print_sudoku();
 
     return 0;
 
